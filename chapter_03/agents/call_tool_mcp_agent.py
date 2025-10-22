1f698d185cd1a776ed333d7421cf7c5f872b2423
ankush.nakaskar <ankush.nakaskar@phonepe.com>
2025-10-22 17:18:09 +0530

    Added changes related to tools call

--- chapter_03/agents/call_tool_mcp_agent.py
+++ chapter_03/agents/call_tool_mcp_agent.py
@@ -7,12 +7,11 @@ from openai import OpenAI
 from chapter_03.stdio import stdio_mcp_client
 from readkey import get_token
 
-api_key= str(get_token())
+api_key = str(get_token())
 print("API key is : ")
 print(api_key)
 http_client = httpx.Client(verify=False)
 client = OpenAI(base_url="https://godric.nixy.stg-drove.phonepe.nb6/v1", api_key=api_key, http_client=http_client)
-
 
 mcp_client = stdio_mcp_client.MCPClient(
     name="calculator_server_connection",
@@ -28,18 +27,41 @@ mcp_client = stdio_mcp_client.MCPClient(
 print("Welcome to your AI Assistant. Type 'goodbye' to quit.")
 
 
-def call_open_ai_llm(prompts,available_tools):
+def call_open_ai_llm(prompts, available_tools):
     completion = client.chat.completions.create(
         model="global:LLM_GLOBAL_GPT_4O_MINI_STG",
         store=True,
-        tools=available_tools,
+        tools=[{
+            "type": "function",
+            "function": {
+                "name": "Adding",
+                "description": "Adding two values",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "a": {
+                            "type": "number",
+                            "description":
+                                "Aurgument A",  # 5
+                        },
+                        "b": {
+                            "type": "number",
+                            "description":
+                                "Aurgument B"  # 6
+                        }
+                    }
+
+                }
+            }
+        }],
         messages=[
-    {
-        "role": "user",
-        "content": prompts
-    }
-]
+            {
+                "role": "user",
+                "content": prompts
+            }
+        ]
     )
+    print(completion)
     functions = []
     for choice in completion.choices:
         for completion_message in choice.message.tool_calls:
@@ -47,21 +69,19 @@ def call_open_ai_llm(prompts,available_tools):
     return functions;
 
 
-
-
-
 async def main():
     """Main async function to run the assistant."""
     await mcp_client.connect()
     available_tools = await mcp_client.get_available_tools()
-    print(f"Available tools: {", ".join([tool['name'] for tool in available_tools])}")
 
+    print(f"Available tools: {", ".join([tool['name'] for tool in available_tools])}")
+    print(available_tools)
     while True:
         prompt = input("You: ")
         if prompt.lower() == "goodbye":
             print("AI Assistant: Goodbye!")
             break
-        message =call_open_ai_llm(prompt,available_tools=available_tools)
+        message = call_open_ai_llm(prompt, available_tools=available_tools)
         print(f"Assistant: {message}")
     await mcp_client.disconnect()
 
