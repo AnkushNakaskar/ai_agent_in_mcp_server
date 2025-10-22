3471d5e8d66ed16412de4c85cd8b2f40e8c658fd
ankush.nakaskar <ankush.nakaskar@phonepe.com>
2025-10-22 15:42:17 +0530

    Added basic for mcp client but actual connection is missing

--- chapter_03/mcp_agent.py
+++ chapter_03/mcp_agent.py
@@ -1,6 +1,9 @@
 from openai import OpenAI
 import httpx
+import asyncio
+from pathlib import Path
 
+from chapter_03.stdio.stdio_mcp_client import MCPClient
 from readkey import get_token
 import httpx
 from openai import OpenAI
@@ -12,6 +15,21 @@ print("API key is : ")
 print(api_key)
 http_client = httpx.Client(verify=False)
 client = OpenAI(base_url="https://godric.nixy.stg-drove.phonepe.nb6/v1", api_key=api_key, http_client=http_client)
+
+
+mcp_client = MCPClient(
+    name="calculator_server_connection",
+    command="uv",
+    server_args=[
+        "--directory",
+        str(Path(__file__).parent.parent.resolve()),
+        "run",
+        "calculator_server.py",
+    ],
+)
+
+print("Welcome to your AI Assistant. Type 'goodbye' to quit.")
+
 
 def call_open_ai_llm(prompts):
     completion = client.chat.completions.create(
@@ -28,7 +46,6 @@ def call_open_ai_llm(prompts):
 
 
 
-print("Welcome to your AI Assistant. Type 'goodbye' to quit.")
 
 while True:
     prompt = input("You: ")
@@ -51,3 +68,22 @@ while True:
     # print(message)
     if message and message.content :
         print(f"Assistant: {message.content}")
+
+
+
+async def main():
+    """Main async function to run the assistant."""
+    await mcp_client.connect()
+    while True:
+        prompt = input("You: ")
+        if prompt.lower() == "goodbye":
+            print("AI Assistant: Goodbye!")
+            break
+        message =call_open_ai_llm(prompt)
+        if message and message.content:
+            print(f"Assistant: {message.content}")
+    await mcp_client.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
